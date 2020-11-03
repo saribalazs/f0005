89df86bcbd4a597584cbbc99c711d80f9e2f1d84
saribalazs <71698313+saribalazs@users.noreply.github.com>
2020-11-03 14:01:11 +0100

    Add files via upload

---
+++ f0005c.py
@@ -0,0 +1,11 @@
+gondolt_szám = 73
+tipp = input('Szerinted melyik számra gondolok 1 és 100 között? ')
+tipp = int(tipp)
+gondolt_szám2 = 37
+tipp2 = input('Szerinted melyik számra gondolok 1 és 100 között? ')
+tipp2 = int(tipp2)
+if gondolt_szám2 == tipp2:
+    print('eltaláltad!')
+    print('szép volt!')
+else:
+    print('jaj de hülye vagy.')
